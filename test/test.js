0cea39c99f4119925f8033a24736c80108e19d3d
kevin grozav <Kevin.Grozav@gmail.com>
2017-12-04 22:37:50 -0800

    Update to async/await; Fix linting errors

--- test/test.js
+++ test/test.js
@@ -22,9 +22,8 @@ describe('Suite two: Basic gets', () => {
 });
 
 describe('Suite three: Scraper test', () => {
-    it('getAlbums() gives back a JSON array', () => {
-        scraper.getAlbums('Taylor Swift').then((result) => {
-            assert.notEqual(undefined, result.length);
-        });
+    it('getAlbums() gives back a JSON array', async () => {
+        const result = await scraper.getAlbums('Taylor Swift');
+        assert.notEqual(undefined, result.length);
     });
 });
