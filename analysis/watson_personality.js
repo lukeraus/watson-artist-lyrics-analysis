657851f8c208b3ac859601a46297b24509b6b55d
Luke Raus <Luke.Raus@ibm.com>
2017-12-08 15:31:00 -0800

    Get personality insights for all albums

--- analysis/watson_personality.js
+++ analysis/watson_personality.js
@@ -6,30 +6,42 @@ const config = JSON.parse(fs.readFileSync('./analysis/credentials.json'));
 const personalityInsights = new PersonalityInsightsV3({ username: config.username, password: config.password, version_date: '2016-10-19' });
 
 
-exports.getPeronalityInsights = async (artistLyrics, artistName) => {
+exports.getPeronalityInsights = async (artistLyrics, artistName, albumTitle) => {
     personalityInsights.profile({
         text: artistLyrics,
         consumption_preferences: true
     }, (err, response) => {
         if (err) { throw err; }
-        const fileName = `./analysis/artists_results/${artistName.toLowerCase().split(' ').join('')}.json`;
+        const albumTitleCompress = albumTitle.toLowerCase().split(' ').join('').replace('/', '-');
+        const artistNameCompress = artistName.toLowerCase().split(' ').join('');
+        const albumDir = `./analysis/artists_results/${artistNameCompress}`;
+        if (!fs.existsSync(albumDir)) {
+            fs.mkdirSync(albumDir);
+        }
+        const fileName = `./analysis/artists_results/${artistNameCompress}/${albumTitleCompress}.json`;
         fs.writeFile(fileName, JSON.stringify(response, null, 4), (error) => {
           if (error) throw error;
           console.log(`File completed: ${fileName}`);
         });
     });
-  };
+};
 
-const test = async (lyricsJSON) => {
+const test = async (lyricsFileJSON) => {
     try {
-        const contents = fs.readFileSync(lyricsJSON);
+        const contents = fs.readFileSync(lyricsFileJSON);
         // Define to JSON type
-        const jsonContent = JSON.parse(contents);
+        const lyricsJSON = JSON.parse(contents);
 
-        // TODO: get insights on whole album
-        // This is just to get the insights on the first song of the first album
-        const lyricsToAnalyize = jsonContent.albums[0].songs[0].lyrics;
-        await exports.getPeronalityInsights(lyricsToAnalyize, jsonContent.artist);
+        console.log(`Getting analysis on Artist: ${lyricsJSON.artist}`);
+        for (let i = 0; i < lyricsJSON.albums.length; i++) {
+            const albumTitle = lyricsJSON.albums[i].album;
+            let albumLyrics = '';
+            for (let x = 0; x < lyricsJSON.albums[i].songs.length; x++) {
+                albumLyrics += lyricsJSON.albums[i].songs[x].lyrics;
+            }
+            console.log(`Getting analysis on album: ${albumTitle}`);
+            exports.getPeronalityInsights(albumLyrics, lyricsJSON.artist, albumTitle);
+        }
     } catch (rejectionError) {
         throw rejectionError;
     }
