f650d9412ed678b434b71a15b70a4f38b53be5a5
Timothy Castillo <Timothy.Castillo@ibm.com>
2018-02-13 11:59:56 -0800

    pushing up a legend and started hover interactions. will add tooltips later

--- src/InsightLines.js
+++ src/InsightLines.js
@@ -28,7 +28,7 @@ class InsightLines extends Component {
 
 		this.yScale = d3.scaleLinear()
 			.domain([0, 1])
-			.range([360, 40]);
+			.range([380, 80]);
 
 		this.xScale = d3.scaleTime()
 			.domain(this.props.xScale.domain())
@@ -38,43 +38,85 @@ class InsightLines extends Component {
 			.x(d => this.xScale(d.releaseDate))
 			.y(d => this.yScale(d.percentile));
 
+		this.legendScale = d3.scaleLinear()
+			.domain([0, Object.keys(this.insightData).length])
+			.range([this.props.rowWidth * 0.1, this.props.rowWidth * 0.9]);
+
 		this.color = d3.scaleOrdinal(d3.schemeCategory10);
+
+		this.hovered = undefined;
 	}
 
 	componentDidMount() {
+		this.svg = d3.select('.insights-svg');
+		this.lines = this.svg.append('g').classed('lines', true);
+		this.circles = this.svg.append('g').classed('circles', true);
+
+		Object.keys(this.insightData).forEach((trait) => {
+			const className = `${trait.split(' ').join('').toLowerCase()}`;
+
+			this.circles.selectAll(`${className}-circle`)
+				.data(this.insightData[trait]).enter().append('circle')
+					.classed(`${className}-circle`, true)
+					.classed('insight-circle', true)
+					.attr('r', 7)
+					.attr('stroke-width', 3)
+					.attr('stroke', () => this.color(trait))
+					.attr('fill', 'white')
+					.attr('cx', d => this.xScale(d.releaseDate))
+					.attr('cy', d => this.yScale(d.percentile))
+					.on('mouseover', function mouseover() {
+						d3.select(this).transition()
+							.attr('r', 9)
+							.attr('stroke-width', 4);
+					})
+					.on('mousemove', () => {})
+					.on('mouseout', function mouseout() {
+						d3.select(this).transition()
+							.attr('r', 7)
+							.attr('stroke-width', 3);
+					});
+
+			this.lines.append('path')
+				.classed(`${className}-line`, true)
+				.classed('line', true)
+				.attr('stroke', () => this.color(trait))
+				.attr('d', this.line(this.insightData[trait]));
+		});
 	}
 
-	shouldComponentUpdate(props) {
+	componentWillUpdate(props) {
 		this.xScale.range([0, props.rowWidth]);
-		return true;
+		this.legendScale.range([props.rowWidth * 0.1, props.rowWidth * 0.9]);
+
+		Object.keys(this.insightData).forEach((trait) => {
+			const className = trait.split(' ').join('').toLowerCase();
+			this.svg.selectAll(`.${className}-circle`)
+				.attr('cx', d => this.xScale(d.releaseDate))
+				.attr('cy', d => this.yScale(d.percentile));
+
+			this.lines.select(`.${className}-line`)
+				.attr('d', this.line(this.insightData[trait]));
+		});
+	}
+
+	mouseOver = (ev) => {
+		console.log(ev.target);
+		this.hovered = ev.target;
 	}
 
 	render() {
-		const lines = Object.keys(this.insightData).map(trait => (
-			<path
-				className="line"
-				d={this.line(this.insightData[trait])}
-				stroke={this.color(trait)}
-				key={trait}
-			/>
-		));
-
-		const circles = Object.keys(this.insightData).reduce((currentCircles, trait) => {
-			const insights = this.insightData[trait];
-
-			return currentCircles.concat(insights.map(insight => (
-				<circle
-					className="circle"
-					r="10"
-					fill="white"
-					strokeWidth="5"
-					key={`${trait}-${insight.album}`}
-					cx={this.xScale(insight.releaseDate)}
-					cy={this.yScale(insight.percentile)}
-					stroke={this.color(trait)}
-				/>
-			)));
-		}, []);
+		const legend = Object.keys(this.insightData).map((trait, i) => {
+			const style = {
+				transform: `translate(${this.legendScale(i)}px, 32px)`
+			};
+			return (
+				<g style={style} key={trait}>
+					<rect width="16" height="16"fill={this.color(trait)} />
+					<text x="22" y="14">{trait}</text>
+				</g>
+			);
+		});
 
 		const [start, stop] = this.yScale.domain();
 		const labels = d3.range(start, stop + 0.01, 0.2).map(value => (
@@ -93,20 +135,13 @@ class InsightLines extends Component {
 					preserveAspectRatio="xMaxYMin"
 					className="insights-svg"
 				>
-					{labels}
+					<g>{labels}</g>
+					<g>{legend}</g>
 					<rect className="clear" />
-					<g className="lines">
-						{lines}
-					</g>
-					<g className="circles">
-						{circles}
-					</g>
 				</svg>
 			</div>
 		);
 	}
 }
-// viewBox={`0 0 100 ${this.yScale.range().pop()}`}
-// preserveAspectRatio="xMinYMin"
 
 export default InsightLines;
