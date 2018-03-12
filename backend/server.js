9082d101143944a08b37b29b89d1c0b9e1cd5914
Luke Raus <Luke.Raus@ibm.com>
2018-03-11 23:36:56 -0700

    Whoops thats not how error callbacks work

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,21 @@ MongoClient.connect(dbUrl, (err, client) => {
 }); // end of mongo client
 
 
-
 /* express routes and listen */
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+function internalServerError(req, res, err) {
+  let jsonResponse = {};
+  console.log('Internal error in search: ' + err);
+  res.status(500);
+  jsonResponse.type = 'SERVER_ERROR';
+  jsonResponse.message = 'Intenral Server error'
+  res.send(JSON.stringify(jsonResponse, null, 3));
+}
 
 // GET method routes
 app.get('/hello', (req, res) => {
@@ -33,6 +41,39 @@ app.get('/hello', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('/artistList', (req, res) => {
+  console.log('Entering artistList....');
+  console.log('Request: ' + JSON.stringify(req.body));
+  
+  // Set boilerplate response
+  res.setHeader('Content-Type', 'application/json');
+  let jsonResponse = {
+    type: 'DEFAULT',
+    message: 'None'
+  };
+  
+  const fetch = db.collection(collectionName).find({}).toArray((err, result) => {
+    // DB Error
+    if (err) {
+      internalServerError(req, res, err);
+    }
+
+    // Found list
+    let artistNameList = [];
+    for (let data of result) {
+      if (data.artist && data.artist.name) {
+        let artistName = data.artist.name;
+        artistNameList.push(artistName);
+      }
+    }
+    res.status(200);
+    jsonResponse.type = 'ARTIST_LIST';
+    jsonResponse.message = artistNameList;
+    res.send(JSON.stringify(jsonResponse, null, 3));
+  });
+
+}); // end of /artistList
 
 // POST method routes
 app.post('/search', function (req, res) {
@@ -59,55 +100,45 @@ app.post('/search', function (req, res) {
   let searchTerm = req.body.search;
   const query = { "artist.name": searchTerm };
   
-  try {
-    db.collection(collectionName).findOne(query, (err, storedArtist) => {
-      console.log(`in collection ${collectionName} result: ${storedArtist}`);
+  db.collection(collectionName).findOne(query, (err, storedArtist) => {
+    console.log(`in collection ${collectionName} result: ${storedArtist}`);
 
-      // For DB error
-      if (err) {
-        throw err;
-      }
+    // For DB error
+    if (err) {
+      internalServerError(req, res, err);
+    }
 
-      // Send back artist result if found in DB
-      if (storedArtist) {
-        delete storedArtist._id;
-        console.log(`Sending Stored Artists: ${JSON.stringify(storedArtist.artist.name)}`);
-        res.status(200);
-        jsonResponse.type = 'ARTIST_FOUND';
-        jsonResponse.message = storedArtist;
-        res.send(JSON.stringify(jsonResponse, null, 3));
-        return;
-        
-      } else {
-        // if not in DB, look on spotify
-        console.log('artist not found in db');
-        console.log('Searching spotify for: ' + searchTerm);
-        spotifySearch.search(searchTerm).then(function(found) {
-          if (found) {
-            res.status(200);
-            jsonResponse.type = 'ARTIST_FOUND_NO_WATSON';
-            jsonResponse.message = 'Found artist ' + searchTerm +
-              ' on spotify, but Watson has not analyzed their lyrics.' +
-              ' Please enter your email and we will let you know when we have that artist.';
-            res.send(JSON.stringify(jsonResponse, null, 3));
-          } else {
-            res.status(404);
-            jsonResponse.type = 'ARTIST_NOT_FOUND';
-            jsonResponse.message = 'Could not find: ' + searchTerm + ' on spotify. Please try again';
-            res.send(JSON.stringify(jsonResponse, null, 3));
-          }
-        }); // end of spotify search
-      }
-    }); // end of seach
-
-  } catch (err) {
-    // In case something goes wrong
-    console.log('Internal error in search: ' + err);
-    res.status(500);
-    jsonResponse.type = 'SERVER_ERROR';
-    jsonResponse.message = 'Intenral Server error'
-    res.send(JSON.stringify(jsonResponse, null, 3));
-  }
+    // Send back artist result if found in DB
+    if (storedArtist) {
+      delete storedArtist._id;
+      console.log(`Sending Stored Artists: ${JSON.stringify(storedArtist.artist.name)}`);
+      res.status(200);
+      jsonResponse.type = 'ARTIST_FOUND';
+      jsonResponse.message = storedArtist;
+      res.send(JSON.stringify(jsonResponse, null, 3));
+      return;
+      
+    } else {
+      // if not in DB, look on spotify
+      console.log('artist not found in db');
+      console.log('Searching spotify for: ' + searchTerm);
+      spotifySearch.search(searchTerm).then(function(found) {
+        if (found) {
+          res.status(200);
+          jsonResponse.type = 'ARTIST_FOUND_NO_WATSON';
+          jsonResponse.message = 'Found artist ' + searchTerm +
+            ' on spotify, but Watson has not analyzed their lyrics.' +
+            ' Please enter your email and we will let you know when we have that artist.';
+          res.send(JSON.stringify(jsonResponse, null, 3));
+        } else {
+          res.status(404);
+          jsonResponse.type = 'ARTIST_NOT_FOUND';
+          jsonResponse.message = 'Could not find: ' + searchTerm + ' on spotify. Please try again';
+          res.send(JSON.stringify(jsonResponse, null, 3));
+        }
+      }); // end of spotify search
+    }
+  }); // end of seach
 
 }); // end of post /search
 
@@ -134,42 +165,37 @@ app.post('/startDataRetriever', function (req, res) {
   let searchTerm = req.body.search;
   let email = req.body.email;
 
-  try {
-    // Run dataRetriever async
-    dataRetriever.run(searchTerm).then(function(artistResult) {
-      // Store inside database
-      db.collection(collectionName).save(artistResult, (err, dbRes) => {
-        if (err) {
-          throw err;
-        } else {
-          console.log('Successfully stored artistResults into collection');
-          // Send mail success
-          console.log('Sending email to: ' + email);
-          var mailOptions = {
-            from: 'spacejam2042@gmail.com',
-            to: email,
-            subject: 'Watson Lyrics analyzer done!',
-            text: 'Succefully analyized: ' + searchTerm
-          };
-          emailClient.sendMail(mailOptions);
-        }
-      });
-    }); // end of dataRetriever
-
-  } catch (err) {
-    // In case something goes wrong
-    console.log('Error running dataRetriver: ' + err);
-
-    // Send mail failure
-    console.log('Sending failure email to: ' + email);
-    var mailOptions = {
-      from: 'spacejam2042@gmail.com',
-      to: email,
-      subject: 'Watson Lyrics analyzer failed',
-      text: 'Watson could not analyize lyrics for: ' + searchTerm + '. Please try again.'
-    };
-    emailClient.sendMail(mailOptions);
-  }
+  // Run dataRetriever async
+  dataRetriever.run(searchTerm).then(function(artistResult) {
+    // Store inside database
+    db.collection(collectionName).save(artistResult, (err, dbRes) => {
+      if (err) {
+        // In case something goes wrong
+        console.log('Error running dataRetriver: ' + err);
+        // Send mail failure
+        console.log('Sending failure email to: ' + email);
+        var mailOptions = {
+          from: 'spacejam2042@gmail.com',
+          to: email,
+          subject: 'Watson Lyrics analyzer failed',
+          text: 'Watson could not analyize lyrics for: ' + searchTerm + '. Please try again.'
+        };
+        emailClient.sendMail(mailOptions);
+     
+      } else {
+        console.log('Successfully stored artistResults into collection');
+        // Send mail success
+        console.log('Sending email to: ' + email);
+        var mailOptions = {
+          from: 'spacejam2042@gmail.com',
+          to: email,
+          subject: 'Watson Lyrics analyzer done!',
+          text: 'Succefully analyized: ' + searchTerm
+        };
+        emailClient.sendMail(mailOptions);
+      }
+    });
+  }); // end of dataRetriever
 
   console.log('Running DataRetriever for: ' + searchTerm);
   res.status(200);
